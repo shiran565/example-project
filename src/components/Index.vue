a887243100889305e834cc3bb25ee8c0436a727d
shiran565 <gewei565@gmail.com>
2016-05-08 22:16:42 +0800

    clipboard添加

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,4 +1,27 @@
 <template>
 	<h1>index</h1>
 
+
+		<!-- Target -->
+	<input id="foo" value="https://github.com/zenorocha/clipboard.js.git">
+
+	<!-- Trigger -->
+	<button class="btn" data-clipboard-target="#foo">Copy to clipboard
+	</button>
+
 </template>
+
+<script type="text/javascript">
+	var Clipboard = require("clipboard");
+
+	var clipboard = new Clipboard('.btn');
+
+	clipboard.on('success', function(e) {
+
+	    console.info('Action:', e.action);
+	    console.info('Text:', e.text);
+	    console.info('Trigger:', e.trigger);
+
+	    e.clearSelection();
+	});
+</script>
